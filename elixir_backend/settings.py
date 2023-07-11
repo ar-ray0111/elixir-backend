855cd90661daa64946a02a3afdcb1024f40d111f
ar-ray0111 <arjunomray@gmail.com>
2023-07-11 16:03:59 +0530

    preparing for vercel

--- elixir_backend/settings.py
+++ elixir_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h-pqxf*6@h$s)&ia*@smwhiid7!oozc99)s-jbcnkyh57)hbee
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','.vercel.app']
 
 
 # Application definition
@@ -76,9 +76,14 @@ WSGI_APPLICATION = 'elixir_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': os.getenv('POSTGRES_URL'),
+        'NAME': os.getenv('PGNAME'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
     }
 }
 
